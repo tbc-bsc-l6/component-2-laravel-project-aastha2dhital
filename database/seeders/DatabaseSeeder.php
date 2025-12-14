79f486fde8fb9ee42adb2dcf676d5110a6a7774d
daastha23 <daastha23@tbc.edu.np>
2025-12-14 14:22:51 +0545

    Add user_roles migration and setup DatabaseSeeder structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,19 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Seed user roles (admin, teacher, student, old_student)
+        $this->call(RoleSEeder::class);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Seed admin user (ONLY created through seeder)
+        @this->call(AdminSeeder::class);
     }
 }
